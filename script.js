17e4dea91be2c74a33154b9ac9a94a13584f5f86
Zephiroooo <lloydmaster.43@gmail.com>
2026-02-08 20:07:27 +0800

    script.js

--- script.js
+++ script.js
@@ -56,14 +56,15 @@
 const messages = [
     "Are you sure?",
     "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "Sayangggg naman",
+    "Please bebuuuu",
+    "Labyuuuu man hihi",
+    "Ma sad man bebuuu!!",
+    "Kababaeng tao humihindi!",
+    "Pag ako namatai",
+    "Bawal mag nooooo",
+    "Yes na kawww!!❤️"
+    "Pag nag no, ibig sabihin fuck"
 ];
 
 let messageIndex = 0;
